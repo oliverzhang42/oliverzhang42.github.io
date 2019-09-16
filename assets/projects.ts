5f6f009ea3d89e7a2418d5b84db43cf56a5f2320
oliverzhang42 <oliver.zhang.42@gmail.com>
2019-09-15 20:00:45 -0700

    Auto-generated commit

---
+++ assets/projects.ts
@@ -0,0 +1,26 @@
+export const projects = [
+  {
+    name: 'Dogs VS. Cats',
+    type: 'Classification',
+    desc: '97% Accuracy, 0.0988 loss, equivalent to 422nd place out of 1314 competitors Used a transfer learning with a pretrained ResNet50 model, applied data augmentation and pseudo labeling using keras',
+    image: 'https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80'
+  },
+  {
+    name: 'Convnet Visualization',
+    type: 'Visualization',
+    desc: 'My algorithm, in white, was playing another algorithm, shown in black. (The green squares are the empty board). My algorithm has learned the strategy of giving up the bottom most row in favor of the second-to-last row.',
+    image: 'https://images.unsplash.com/photo-1562887245-f2d5024f6134?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80'
+  },
+  {
+    name: 'Reinforcement Learning',
+    type: 'Game',
+    desc: 'Implemented tool for visualizing ConvNets Used CAM, guided saliency, DeepSHAP, and integrated gradients techniques Helped validate the network\'s predictions and build confidence in the model.',
+    image: 'https://images.unsplash.com/photo-1496312093257-f869e5d95ce6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80'
+  },
+  {
+    name: 'Deep Generative Models',
+    type: 'Classification',
+    desc: 'The CelebA dataset is a set of celebrity faces. My model is trying to generate images that mimic these faces.',
+    image: 'https://images.unsplash.com/photo-1496312093257-f869e5d95ce6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80'
+  }
+];
